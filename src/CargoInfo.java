2de0c586e83a631cfb9f1a469fbc1b5e6fea4b62
Kamron Khamrakulov <khamrakulovkamron@gmail.com>
2023-08-03 16:49:15 +0500

    CargoInfo changed to immutable #2

--- src/CargoInfo.java
+++ src/CargoInfo.java
@@ -2,9 +2,25 @@ public class CargoInfo {
     Dimensions dimensions; // габариты
     int weight; // масса
     String address; // адрес
+
     boolean property; // свойство — можно ли переворачивать
     String id; // регистрационный номер (может содержать буквы)
     boolean isFragile; // является ли груз хрупким
+
+    public CargoInfo(Dimensions dimensions,
+                     int weight,
+                     String address,
+                     boolean property,
+                     String id,
+                     boolean isFragile
+    ) {
+        this.dimensions = dimensions;
+        this.weight = weight;
+        this.address = address;
+        this.property = property;
+        this.id = id;
+        this.isFragile = isFragile;
+    }
 
     public Dimensions getDimensions() {
         return dimensions;
@@ -28,5 +44,17 @@ public class CargoInfo {
 
     public void setAddress(String address) {
         this.address = address;
+    }
+
+    public boolean isProperty() {
+        return property;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public boolean isFragile() {
+        return isFragile;
     }
 }
