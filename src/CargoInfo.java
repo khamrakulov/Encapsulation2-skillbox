aa76f63732de737ff2e770d3647eaa70c7b9da4d
Kamron Khamrakulov <khamrakulovkamron@gmail.com>
2023-08-03 16:39:03 +0500

    dimensions changed

---
+++ src/CargoInfo.java
@@ -0,0 +1,9 @@
+public class CargoInfo {
+
+
+    int weight; // масса
+    String address; // адрес
+    boolean property; // свойство — можно ли переворачивать
+    String id; // регистрационный номер (может содержать буквы)
+    boolean isFragile; // является ли груз хрупким
+}
